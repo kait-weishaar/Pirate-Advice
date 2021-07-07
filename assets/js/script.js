c5d01e03b26369f113315ac7aa13c78c923e9c3c
Kait <kweishaar@hawk.iit.edu>
2021-07-06 21:40:32 -0500

    some js for initial page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const searchHistUL = document.querySelector(`#search-history`);
+const imgContainerEl = document.getElementById(`img-container`);
+const btnContainerEl = document.getElementById(`button-container`);
 
 const getAdvice = function(searchTerm){
     fetch(`https://api.adviceslip.com/advice/search/${searchTerm}`).then(function(response) {
@@ -71,8 +73,27 @@ loadSearch();
 getAdvice(`spiders`);
 // `spiders` will change to user input, and it will be an event listener
 
+let displayInitialPage = function() {
+//get giphy through api call ---Dylan?
+//display giphy to page --Z? reference in class giphy activity
+let heroImg = document.createElement('img');
+heroImg.setAttribute("src", "put URL here?");
+
+//create searchbar
+let form = document.createElement('form');
+let searchBar = document.createElement("input");
+    searchBar.setAttribute("type", "search");
+    searchBar.setAttribute("name", "searchTerm");
+    searchBar.setAttribute("placeholder", "Search");
+let searchBtn =  document.createElement("input"); 
+   searchBtn.setAttribute("type", "button");  
+   form.appendChild(searchBar);
+   form.appendChild(searchBtn);
 
 
+};
+
+searchBtn.addEventListener("click", getAdvice);
 //display initial page
         //searchbar 
         //pirate chest image
